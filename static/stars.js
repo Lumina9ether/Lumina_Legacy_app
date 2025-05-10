e9073feb42b5c168832672bcafb3a19b0d47fb56
Lumina9ether <highvibrationbeings@gmail.com>
2025-05-10 12:36:52 -0400

    stars.js

--- static/stars.js
+++ static/stars.js
@@ -1,1 +1,35 @@
+const canvas = document.getElementById('stars');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let stars = Array.from({ length: 100 }, () => ({
+  x: Math.random() * canvas.width,
+  y: Math.random() * canvas.height,
+  radius: Math.random() * 1.5,
+  velocity: Math.random() * 0.2
+}));
+
+function drawStars() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  stars.forEach(star => {
+    ctx.beginPath();
+    ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
+    ctx.fill();
+  });
+  moveStars();
+  requestAnimationFrame(drawStars);
+}
+
+function moveStars() {
+  stars.forEach(star => {
+    star.y += star.velocity;
+    if (star.y > canvas.height) {
+      star.y = 0;
+      star.x = Math.random() * canvas.width;
+    }
+  });
+}
+drawStars();
 
